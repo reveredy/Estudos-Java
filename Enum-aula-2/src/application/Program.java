0d6ccdb478ea3bbf334a6c16817aedba68b9dfe2
reveredy <carlos.andre.albanus@hotmail.com>
2022-02-11 01:38:55 -0300

    Pastas e exercicios sobre enum em Java

---
+++ Enum-aula-2/src/application/Program.java
@@ -0,0 +1,66 @@
+package application;
+
+import entities.Department;
+import entities.HourContract;
+import entities.Worker;
+import entities.enums.WorkerLevel;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) throws ParseException {
+        Locale.setDefault(Locale.US);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter department's name: ");
+        String departmentName = sc.nextLine();
+
+        System.out.println("Enter worker data: ");
+
+        System.out.println("Name: ");
+        String workerName = sc.nextLine();
+
+        System.out.println("Level: ");
+        String workerLevel = sc.nextLine();
+
+        System.out.println("Base Salary: ");
+        double baseSalary = sc.nextDouble();
+
+        Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
+
+        System.out.println("How many contracts to this worker: ");
+        int nContracts = sc.nextInt();
+
+        for (int i = 1; i <= nContracts; i++){
+            System.out.println("Enter contract #" + i + " data: ");
+            System.out.print("Date (DD/MM/YYY): ");
+            Date contractDate = sdf.parse(sc.next());
+
+            System.out.println("Value per Hour: ");
+            double valuePerHour = sc.nextDouble();
+
+            System.out.println("Duration (hours): ");
+            int hours = sc.nextInt();
+
+            HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+            worker.addContract(contract);
+        }
+
+        System.out.println();
+        System.out.println("Enter month and year to calculate income (MM:YYYY): ");
+        String monthAndYear = sc.next();
+
+        int month = Integer.parseInt(monthAndYear.substring(0, 2));
+        int year = Integer.parseInt(monthAndYear.substring(3));
+
+        System.out.println("Name: " + worker.getName());
+        System.out.println("Department: " + worker.getDepartment().getName());
+        System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
+    }
+}
---
+++ Enums/src/application/OrderStatus.java
@@ -0,0 +1,8 @@
+package application;
+
+public enum OrderStatus {
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+}
